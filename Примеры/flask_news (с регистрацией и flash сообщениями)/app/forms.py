a7d215e4d57253e6cf330ae6a1dd43bb6007c1e3
dmitry <windn12@gmail.com>
2024-04-20 14:04:59 +0500

    Upload 2024

---
+++ Примеры/flask_news (с регистрацией и flash сообщениями)/app/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Optional, Email, EqualTo
+
+from .models import Category
+
+
+def get_categories():
+    categories = Category.query.all()
+    return [(category.id, category.title) for category in categories]
+
+
+class NewsForm(FlaskForm):
+    title = StringField('Название', validators=[DataRequired(message="Поле не должно быть пустым"),
+                                                Length(max=255, message='Введите заголовок длиной до 255 символов')])
+    text = TextAreaField('Текст', validators=[DataRequired(message="Поле не должно быть пустым")])
+    category = SelectField('Категория', choices=get_categories, validators=[Optional()])
+    submit = SubmitField('Добавить')
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Имя пользователя", validators=[DataRequired()])
+    password = PasswordField("Пароль", validators=[DataRequired()])
+    remember = BooleanField("Запомнить меня")
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Имя пользователя", validators=[DataRequired()])
+    name = StringField("Имя", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email(message='Некорректный email')])
+    password = PasswordField("Пароль", validators=[DataRequired()])
+    password2 = PasswordField("Повторите пароль",
+                              validators=[DataRequired(), EqualTo('password', message='Пароли не совпадают')])
+    submit = SubmitField('Зарегистрироваться')
